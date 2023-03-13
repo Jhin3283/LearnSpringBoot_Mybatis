2216920d7ea1cbffc6dfe992b17c01ec3830fec6
jhin3283 <hj3283@gmail.com>
2023-03-13 09:48:22 +0900

    ADD: connect springBoot to mysql using mybatis

--- demo/src/main/java/com/example/demo/controller/UserProfileController.java
+++ demo/src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,50 +10,41 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.mapper.UserProfileMapper;
 import com.example.demo.model.UserProfile;
-
-import jakarta.annotation.PostConstruct;
 
 @RestController
 public class UserProfileController {
 
-	private Map<String, UserProfile> userMap;
-
-	@PostConstruct
-	public void init() {
-		userMap = new HashMap<String, UserProfile>();
-		userMap.put("1", new UserProfile("1", "홍길동", "111-1111", "부산시 해운대구 양운로3"));
-		userMap.put("2", new UserProfile("2", "Jhin", "111-1231", "부산시 해운대구 양운로2"));
-		userMap.put("3", new UserProfile("3", "Leo", "112-1121", "부산시 해운대구 양운로1"));
+	private UserProfileMapper mapper;
+	public UserProfileController(UserProfileMapper mapper) {
+		super();
+		this.mapper = mapper;
 	}
+
 
 	@GetMapping("/user/{id}")
 	public UserProfile getUserProfile(@PathVariable("id") String id) {
-		return userMap.get(id);
+		return mapper.getUserProfile(id);
 	}
 
 	@GetMapping("/user/all")
 	public List<UserProfile> getUserProfileList() {
-		return new ArrayList<UserProfile>(userMap.values());
+		return mapper.getUserProfileList();
 	}
 
 	@PutMapping("/user/{id}")
 	public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name,
 			@RequestParam("phone") String phone, @RequestParam("address") String address) {
-		UserProfile userProfile = new UserProfile(id, name, phone, address);
-		userMap.put(id, userProfile);
+		mapper.updateUserProfile(id, name, phone, address);
 	}
 	@PostMapping("/user/{id}")
 	public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name,
 			@RequestParam("phone") String phone, @RequestParam("address") String address) {
-		UserProfile userProfile = userMap.get(id);
-		userProfile.setName(name);
-		userProfile.setPhone(phone);
-		userProfile.setAddress(address);
+		mapper.insertUserProfile(id, name, phone, address);
 	}
 	@DeleteMapping("/user/{id}")
 	public void deleteUserProfile(@PathVariable("id") String id) {
-		userMap.remove(id);
-		
+		mapper.deleteUserProfile(id);
 	}
 }
